28d6543b21d96d241e8745b8960752300e4cc32a
Aldo Silvestre <aldosilvestre89@gmail.com>
2022-08-08 16:37:17 -0300

    modify indent styles

---
+++ nvim/lua/plugin-configs/blankline.lua
@@ -0,0 +1,24 @@
+---@diagnostic disable: undefined-global
+vim.opt.list = true
+vim.opt.listchars:append("space: ")
+
+require('indent_blankline').setup {
+  char = "▏",
+  space_char_blankline = " ",
+  show_first_indent_level = false,
+  show_current_context = true,
+  show_current_context_start = true,
+  filetype_exclude = {
+    'help',
+    'git',
+    'markdown',
+    'text',
+    'terminal',
+    'lspinfo',
+    'packer'
+  },
+  buftype_exclude = {
+    'terminal',
+    'nofile'
+  },
+}
